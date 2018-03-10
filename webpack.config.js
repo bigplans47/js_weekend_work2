bb6a95d2116d0cf0d129204d0be932bbbdaadc02
Dan Kiss <flowfast47@gmail.com>
2018-03-09 16:49:07 -0800

    add plugin to load index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  plugins: [
+    new CleanWebpackPlugin(['dist']),   // new line
+    new HtmlWebpackPlugin({
+      title: 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
